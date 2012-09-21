c43c563b045667af90da5eb38bc21d6c90afd21e
Lachlan Toohey <lachlan.toohey@gmail.com>
2012-09-21 18:45:13 +1000

    Changes to make FileUpload work (and look pretty)

--- staging/urls.py
+++ staging/urls.py
@@ -6,6 +6,7 @@ from django.views.generic import DetailView, ListView
 urlpatterns = patterns('',
     url(r'^$', 'staging.views.index'),
     url(r'^progress/(?P<key>\d+)$', 'staging.views.progress'),
+    url(r'^upload_progress/$', 'staging.views.upload_progress'),
     url(r'^auv/import$', 'staging.views.auvimport'),
     url(r'^auv/imported$', 'staging.views.auvimported'),
     url(r'^file/import$', 'staging.views.fileupload'),
--- staging/views.py
+++ staging/views.py
@@ -4,8 +4,13 @@ from django.db import transaction
 from django.core import serializers
 from django import forms
 
-from staging.forms import AUVImportForm
+from staging.forms import AUVImportForm, FileImportForm
 from staging.auvimport import create_structure, structure_string
+from staging.models import Progress
+from staging.extras import UploadProgressCachedHandler
+from staging import tasks
+
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
 
 def index(request):
     context = {}
@@ -14,6 +19,21 @@ def index(request):
     rc = RequestContext(request)
 
     return render_to_response('staging/index.html', context, rc)
+
+def progress(request, key):
+    """Used to return progress of an operation."""
+    context = {}
+    rc = RequestContext(request)
+
+    try:
+        prog = Progress.objects.get(pk=key)
+    except Progress.DoesNotExist:
+        context['percent'] = 40
+    else:
+        context['percent'] = prog.progress
+
+    return render_to_response('staging/progress.json', context, rc)
+    
 
 def auvimport(request):
     errors = ""
@@ -63,9 +83,45 @@ def auvimported(request):
 
     return render_to_response('staging/auvimported.html', context, rc)
 
+# to enable the handler to exist...
+@csrf_exempt
 def fileupload(request):
+    # get a new progress
+    key = Progress.objects.get_new()
+
+    request.upload_handlers.insert(0, UploadProgressCachedHandler(request))
+    return _fileupload(request)
+
+@csrf_protect
+def _fileupload(request):
     context = {}
     rc = RequestContext(request)
+
+    if request.method == 'POST':
+        form = FileImportForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            # this is where the import is performed...
+            # and the file is read in
+            upload = request.FILES['upload_file']
+
+            # if it is large, read from the file
+            # else read the entirety into a string
+            try:
+                if hasattr(upload, 'temporary_file_path'):
+                    tasks.json_fload(upload.temporary_file_path())
+                else:
+                    tasks.json_sload(upload.read())
+                
+            except Exception as e:
+                errors = form._errors.setdefault(forms.forms.NON_FIELD_ERRORS, forms.util.ErrorList())
+                errors.append("{0}: {1}".format(e.__class__.__name__, e))
+            else:
+                return redirect('staging.views.fileuploaded')
+    else:
+        form = FileImportForm()
+
+    context['form'] = form
 
     return render_to_response('staging/fileupload.html', context, rc)
 
@@ -74,3 +130,21 @@ def fileuploaded(request):
     rc = RequestContext(request)
 
     return render_to_response('staging/fileuploaded.html', context, rc)
+
+def upload_progress(request):
+    """
+    Return JSON object with information about the progress of an upload.
+    """
+    progress_id = ''
+    if 'X-Progress-ID' in request.GET:
+        progress_id = request.GET['X-Progress-ID']
+    elif 'X-Progress-ID' in request.META:
+        progress_id = request.META['X-Progress-ID']
+    if progress_id:
+        from django.utils import simplejson
+        cache_key = "%s_%s" % (request.META['REMOTE_ADDR'], progress_id)
+        data = cache.get(cache_key)
+        return HttpResponse(simplejson.dumps(data))
+    else:
+        return HttpResponseServerError('Server Error: You must provide X-Progress-ID header or query param.')
+
