116b6baae4dab9436a6f294ee747341a35439922
Lachlan Toohey <lachlan.toohey@gmail.com>
2012-09-24 18:29:11 +1000

    Patches to fix an error in half-finished progress bar work.

--- staging/extras.py
+++ staging/extras.py
@@ -3,9 +3,10 @@ from django.core.cache import cache
 from staging.models import Progress
 
 def update_progress(key, progress):
-    obj = Progress.objects.get(pk=key)
-    obj.progress = progress
-    obj.save()
+    #obj = Progress.objects.get(pk=key)
+    #obj.progress = progress
+    #obj.save()
+    pass
 
 class UploadProgressCachedHandler(FileUploadHandler):
     """
