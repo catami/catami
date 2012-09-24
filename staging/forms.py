e0f2448888f6a05fe9eda497874e7c195ccf8c9c
Daniel Marrable <marrabld@gmail.com>
2012-09-24 09:27:23 +0800

    added for update line

--- staging/forms.py
+++ staging/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from Force.models import Campaign
 
 class AUVImportForm(forms.Form):
+    Campaign.objects.update()
     choices = tuple([(c.short_name, "{0} {1}".format(c.date_start, c.short_name))  for c in Campaign.objects.all()])
     campaign_name = forms.ChoiceField(choices=choices)
     mission_name = forms.CharField(max_length=100)
