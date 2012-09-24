7fa95fe081df880cc0d09db5e4ff94da303ae9fb
Mark Gray <markg@ivec.org>
2012-09-24 15:02:43 +0800

    add campaign now take you to the admin interface

--- Force/views.py
+++ Force/views.py
@@ -13,16 +13,7 @@ def index(request):
     return HttpResponse("Hello World, from the CATAMI team.  We are not up and running yet, you can follow us here for now https://plus.google.com/u/0/b/104765819602128308640/104765819602128308640/posts")
 
 def add_campaign(request):
-    if request.method == 'POST': # If the form has been submitted...
-        form = CampaignForm(request.POST) # A form bound to the POST data
-        if form.is_valid(): # All validation rules pass
-            # Process the data in form.cleaned_data
-            form.save();
-            return redirect('/Force/campaigns') # Redirect after POST
-    else:
-        form = CampaignForm() # An unbound form
-
-    return render(request, '/Force/AddCampaign.html', {'form': form})
+    return redirect('/admin/Force/campaign/add/') # Redirect after POST
 
 def auvdeployments(request):
     latest_auvdeployment_list = AUVDeployment.objects.all()
