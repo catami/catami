a513928bdd60282369110dba97457cd9869e4274
Daniel Marrable <marrabld@gmail.com>
2012-09-25 16:28:54 +0800

    added user login to main site

--- catamiPortal/urls.py
+++ catamiPortal/urls.py
@@ -26,5 +26,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    (r'^accounts/login/$', 'django.contrib.auth.views.login',{'template_name': 'registration/login.html'}),
 
 )
