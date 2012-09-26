307440564a071ff8b7f214a765acd90f4edbfffd
Mark Gray <markg@ivec.org>
2012-09-26 12:08:58 +0800

    Campaign search now looks for all text string elements

--- Force/search_indexes.py
+++ Force/search_indexes.py
@@ -1,14 +1,21 @@
 import datetime
 from haystack import indexes
-from Force.models import Campaign
+from Force.models import Campaign, Deployment
 
 class CampaignIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
-    short_name = indexes.CharField(model_attr='short_name')
-    #description = indexes.TextField(model_attr='description')
  
     def get_model(self):
         return Campaign
+ 
+    def index_queryset(self):
+        return self.get_model().objects.all()
+
+class DeploymentIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+ 
+    def get_model(self):
+        return Deployment
  
     def index_queryset(self):
         return self.get_model().objects.all()
