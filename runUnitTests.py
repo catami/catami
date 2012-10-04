c2ec6f2e0be590b3bb2820dfb44600a78f4277b8
Mat <mathew.wyatt@gmail.com>
2012-10-04 20:52:47 +0800

    Removed non-existing catamiWebPortal import statement

--- runUnitTests.py
+++ runUnitTests.py
@@ -2,7 +2,6 @@ import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'catamiPortal.settings'
 from django.test.utils import setup_test_environment
 setup_test_environment()
-import catamiWebPortal
 
 #==================================================#
 # Add Tests to start here
