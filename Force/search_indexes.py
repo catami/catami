84dd9a25a732430b7eeb0de58e5e98b7ec6c03b2
Mark Gray <markg@ivec.org>
2012-10-25 11:07:18 +0800

    Improved search, including spatial search results for auv/bruv deployments

--- Force/search_indexes.py
+++ Force/search_indexes.py
@@ -7,7 +7,7 @@ Edits :: Name : Date : description
 
 """
 from haystack import indexes
-from Force.models import Campaign, Deployment
+from Force.models import Campaign, AUVDeployment, BRUVDeployment
 
 class CampaignIndex(indexes.SearchIndex, indexes.Indexable):
     """@brief Campaign Index
@@ -22,14 +22,42 @@ class CampaignIndex(indexes.SearchIndex, indexes.Indexable):
         return self.get_model().objects.all()
 
 
-class DeploymentIndex(indexes.SearchIndex, indexes.Indexable):
-    """@brief Deployment Index
+# class DeploymentIndex(indexes.SearchIndex, indexes.Indexable):
+#     """@brief Deployment Index
+
+#     """
+#     text = indexes.CharField(document=True, use_template=True)
+
+#     def get_model(self):
+#         return Deployment
+ 
+#     def index_queryset(self):
+#         return self.get_model().objects.all()
+
+
+class AUVDeploymentIndex(indexes.SearchIndex, indexes.Indexable):
+    """@brief AUVDeployment Index
 
     """
     text = indexes.CharField(document=True, use_template=True)
- 
+    #location = indexes.LocationField(model_attr='start_position')
+
     def get_model(self):
-        return Deployment
+        return AUVDeployment
+ 
+    def index_queryset(self):
+        return self.get_model().objects.all()
+
+
+class BRUVDeploymentIndex(indexes.SearchIndex, indexes.Indexable):
+    """@brief BRUVDeployment Index
+
+    """
+    text = indexes.CharField(document=True, use_template=True)
+    #location = indexes.LocationField(model_attr='start_position')
+
+    def get_model(self):
+        return BRUVDeployment
  
     def index_queryset(self):
         return self.get_model().objects.all()
