3f9a85769b101db3c8d0d9dcaae2f7aaa16fec70
Mark Gray <markg@ivec.org>
2012-12-14 16:03:27 +0800

    Added template filter to front page images, amended image links to use full size urls rather than thumbnails

--- Force/templatetags/force_extras.py
+++ Force/templatetags/force_extras.py
@@ -1,56 +1,47 @@
 import os
 import Image
 from django.template import Library
+from urlparse import urlparse
+import httplib
 
 register = Library()
 
-def scale(max_x, size):
-    " size is a tuple: (image_x,image_y) "
-    x, y = size
-    new_y = (float(max_x) / x) * y
-    return (int(max_x), int(new_y))
+def exists(site, path):
+    conn = httplib.HTTPConnection(site)
+    conn.request('HEAD', path)
+    response = conn.getresponse()
+    conn.close()
+    return response.status == 200
 
-def thumbnail(file, size='150x150'):
-    """
+def websafe_image(image_url):
+    """@brief Returns equivalent PNG/JPEG url if file is TIFF format
+
+
     Examples:
     <img src="object.src.url" alt="original image" />
-    <img src="object.image.src|thumbnail" alt="image resized to default 80x60 format" />
+    <img src="object.image.src|websafe_image" alt="PNG version of image" />
 
-    <img src="object.image.src|thumbnail:"200x150"
-         alt="image resized to 200x150, cropped at 150 height if incorrect aspect ratio" />
 
-    <img src="object.image.src|thumbnail:"200x0"
-         alt="image scaled to 250 width, height not croppped / adjusted if incorrect aspect ratio" />
     """
-    if file:
-        # get size params
-        x, y = [int(x) for x in size.split('x')]
-        #
-        # basename, format = file.path.rsplit('.', 1)
-        # baseurl, _format = file.url.rsplit('.', 1)
-        # new_filename = basename + '_' + size + '.jpg'
-        # new_url = baseurl + '_' + size + '.jpg'
+    if image_url:
+        url_data = urlparse(image_url)
 
-        # # if the new image wasn't already resized..
-        # if not os.path.exists(new_filename):
-        #     image = Image.open(file.path)
+        basepath, _format = url_data.path.rsplit('.', 1)
 
-        #     # scale image
-        #     # image.size returns (a,tuple)
-        #     image_x, image_y = scale(x,image.size)
-        #     # convert to RGB
-        #     if image.mode not in ("L", "RGB"):
-        #         image = image.convert("RGB")
-        #     # 'resize'
-        #     image.thumbnail([image_x, image_y], Image.ANTIALIAS)
-        #     # crop if/to max_height setting
-        #     if image_y > image_x & y is not 0:
-        #         # crop image height
-        #         left, top = 0, 0
-        #         box = (left, top, left+x, top+y)
-        #         image = image.crop(box)
-        #     image.save(new_filename, 'JPEG', quality=70)
-        # return new_url
-    return ''
+        new_url_png = url_data.scheme + '://' + url_data.netloc + basepath + '.png'
+        if exists(url_data.netloc, basepath + '.png'):
+            return new_url_png
+        
+        new_url_jpg = url_data.scheme + '://' + url_data.netloc + basepath + '.jpg'
+        if exists(url_data.netloc, basepath + '.jpg'):
+            return new_url_jpg
 
-register.filter(thumbnail)
+        # png_url = urlparse(new_url_png)
+        # url_address = png_url.scheme + '://' + png_url.netloc
+
+        # if exists(url_address, png_url.path):
+        #     return new_url_png
+
+    return image_url
+
+register.filter(websafe_image)
