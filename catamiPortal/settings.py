c800577a1e4a9680a3c3c2aa9b2dec08349eea6f
Lachlan Toohey <l.toohey@acfr.usyd.edu.au>
2013-02-19 11:43:31 +1100

    Refactor settings to use an inheritable structure with local overrides.

--- catamiPortal/settings.py
+++ catamiPortal/settings.py
@@ -1,313 +1,46 @@
 import os
-from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
-import socket
-# Django settings for catamiPortal project.
 
+## Import our defaults (globals)
 
-if socket.gethostname() == 'catami.ivec.org':
-    DEBUG = TEMPLATE_DEBUG = False
-else:
-    DEBUG = True
-    TEMPLATE_DEBUG = DEBUG
+from catamiPortal.conf.settings.default import *
 
-POSTGIS_VERSION = (1, 5, 2)
-SOUTH_TESTS_MIGRATE = False # To disable migrations and use syncdb instead
-SKIP_SOUTH_TESTS = True # To disable South's own unit tests
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-NOSE_ARGS = [
-                '--with-coverage',
-            #    '--cover-package=Force,accounts,clustering,collection,dbadmintool,features,staging,webinterface'
-            #    '--coverage-exclude=../*migrations*'
-            ]
+## Inherit from environment specifics
 
-# The root directory for staging/local storage.
-STAGING_ROOT_DIR = '/media/water/'
-#REUSE_DB = 1
+DJANGO_CONF = os.environ.get('DJANGO_CONF', 'default')
+if DJANGO_CONF != 'default':
+    module = __import__(DJANGO_CONF, globals(), locals(), ['*'])
+    for k in dir(module):
+        locals()[k] = getattr(module, k)
 
-WMS_URL = "http://localhost:8080/geoserver/wms" # standard config for local host
-#WMS_URL = "http://flounder-virtualbox.local:8080/geoserver/wms" # custom if running test server in virtual machine
-WMS_LAYER_NAME = "catami:Force_image"
-WMS_COLLECTION_LAYER_NAME = "catami:collection_images"
+## Import local settings
 
-ADMINS = (
-     ('Dan Marrable', 'd.marrable@ivec.org'),
-)
+try:
+    from local_settings import *
+except ImportError:
+    import sys, traceback
+    sys.stderr.write("Warning: Can't find the file 'local_settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
+    sys.stderr.write("\nFor debugging purposes, the exception was:\n\n")
+    traceback.print_exc()
 
-MANAGERS = ADMINS
+## Remove disabled apps
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis', #'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'Force', # Or path to database file if using sqlite3.
-        'USER': 'pocock', # Not used with sqlite3.
-        'PASSWORD': 'qwer789ASDF456zxcv123', # Not used with sqlite3.
-        'HOST': '', # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '', # Set to empty string for default. Not used with sqlite3.
-    }
-}
+if 'DISABLED_APPS' in locals():
+    INSTALLED_APPS = [k for k in INSTALLED_APPS if k not in DISABLED_APPS]
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'Australia/Perth'
+    MIDDLEWARE_CLASSES = list(MIDDLEWARE_CLASSES)
+    DATABASE_ROUTERS = list(DATABASE_ROUTERS)
+    TEMPLATE_CONTEXT_PROCESSORS = list(TEMPLATE_CONTEXT_PROCESSORS)
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+    for a in DISABLED_APPS:
+        for x, m in enumerate(MIDDLEWARE_CLASSES):
+            if m.startswith(a):
+                MIDDLEWARE_CLASSES.pop(x)
 
-SITE_ID = 1
+        for x, m in enumerate(TEMPLATE_CONTEXT_PROCESSORS):
+            if m.startswith(a):
+                TEMPLATE_CONTEXT_PROCESSORS.pop(x)
 
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
+        for x, m in enumerate(DATABASE_ROUTERS):
+            if m.startswith(a):
+                DATABASE_ROUTERS.pop(x)
 
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale.
-USE_L10N = True
-
-# If you set this to False, Django will not use timezone-aware datetimes.
-USE_TZ = True
-
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
-
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'd7jk#mjzc!qsz*+j)lnx*j0f+g%a0bej-n=&amp;0)7&amp;g=n!^1iaaf'
-
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-    #'django.template.loaders.eggs.Loader',
-)
-
-TEMPLATE_CONTEXT_PROCESSORS = TCP + (
-    'django.core.context_processors.request',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'waffle.middleware.WaffleMiddleware',
-    #'django.middleware.csrf.CsrfResponseMiddleware',
-    # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
-
-ROOT_URLCONF = 'webinterface.urls'
-
-# Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'catamiPortal.wsgi.application'
-
-SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
-
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os.path.join(SITE_ROOT, '../catamiPortal/templates'),
-)
-
-FIXTURE_DIRS = (os.path.join(SITE_ROOT, '../Force/fixtures'))
-
-AUTHENTICATION_BACKENDS = (
-    'userena.backends.UserenaAuthenticationBackend',
-    'guardian.backends.ObjectPermissionBackend',
-    'django.contrib.auth.backends.ModelBackend',
-)
-
-INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    ''
-    # Uncomment the next line to enable the admin:
-    'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    'django.contrib.admindocs',
-    'django.contrib.gis',
-    'Force',
-    'staging',
-    'jsonapi',
-    'webinterface',
-    'django_jenkins',
-    'dbadmintool',
-    'features',
-    'south',
-    'django_coverage',
-    'accounts',
-    'collection',
-    'guardian',
-    'easy_thumbnails',
-    'django_nose',
-    'clustering',
-    'annotations',
-    'tastypie',
-    'userena',
-    'waffle',
-)
-#    'haystack', # disabled 16th Jan 2013
-
-
-#tasks that jenkins will run on the build server
-JENKINS_TASKS = {
-    'django_jenkins.tasks.run_pylint',
-    'django_jenkins.tasks.with_coverage',
-    'django_jenkins.tasks.run_pep8',
-    'django_jenkins.tasks.django_tests'
-}
-
-PROJECT_APPS = (
-    'Force',
-    'staging', 
-    'webinterface',
-    'dbadmintool',
-    'accounts',
-    'annotations',
-    'collection',
-    'features',
-    'jsonapi',
-    'clustering',
-)
-
-#haystack support
-#HAYSTACK_CONNECTIONS = {
-#    'default': {
-#        'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
-#        'URL': 'http://127.0.0.1:8983/solr'
-#    },
-#}
-
-ANONYMOUS_USER_ID = -1  
-
-EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
-#EMAIL_USE_TLS = True
-#EMAIL_HOST = 'smtp.gmail.com'
-#EMAIL_PORT = 587
-#EMAIL_HOST_USER = 'none@none.none'
-#EMAIL_HOST_PASSWORD = 'none'
-#AUTH_PROFILE_MODULE = "accounts.UserProfile"
-AUTH_PROFILE_MODULE = 'accounts.MyProfile'  
-  
-LOGIN_REDIRECT_URL = '/accounts/%(username)s/'  
-LOGIN_URL = '/accounts/signin/'  
-LOGOUT_URL = '/accounts/signout/'  
-
-USERENA_ACTIVATION_REQUIRED = 0
-
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
-        },
-        'simple': {
-            'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-        },
-    },
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        },
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'simple'
-        },
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'formatter': 'simple',
-            'filename': 'log/catamiPortal.log'
-        }
-    },
-    'loggers': {
-        'root': {
-            'handlers': ['console', 'file'],
-            'level': 'DEBUG',
-        },
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-        'staging': {
-            'handlers': ['console', 'file'],
-            'level': 'DEBUG',
-        },
-        'webinterface': {
-            'handlers': ['console', 'file'],
-            'level': 'DEBUG',
-        },
-        'catamiPortal': {
-            'handlers': ['console', 'file'],
-            'level': 'DEBUG',
-        },
-        'dbadmintool': {
-            'handlers': ['console', 'file'],
-            'level': 'DEBUG',
-        },
-        'Force': {
-            'handlers': ['console', 'file'],
-            'level': 'DEBUG',
-        },
-        'features': {
-            'handlers': ['console', 'file'],
-            'level': 'DEBUG',
-        },
-
-    }
-}
