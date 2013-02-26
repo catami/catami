fbd342a01810c05700e4a33e073be7e1ad5589a7
ariell <ariell.friedman@gmail.com>
2013-02-27 10:00:26 +1100

    improved css and documentation

--- webinterface/static/assets/catami-api/js/collections.js
+++ webinterface/static/assets/catami-api/js/collections.js
@@ -6,14 +6,19 @@
  * @param select_fnc
  * @constructor
  **********************************************************************************************************************/
-function CollectionList(preview_fnc, select_fnc) {
+var collection_global_config = {
+    api_baseurl : '/api/dev/collection/',
+    linkurl : "/collections/",
+    preview_fnc : false,
+    select_fnc : false
+};
 
-    /* Class globals
-     ******************************************************************************************************************/
-    var api_baseurl = '/api/dev/collection/?format=json';
-    var linkurl = '/collections/';
-    var preview_fnc = ((typeof preview_fnc !== 'undefined') ? preview_fnc : false);
-    var select_fnc = ((typeof select_fnc !== 'undefined') ? select_fnc : false);
+function CollectionList(settings) {
+
+    // Set default config params
+    var config = jQuery.extend({},collection_global_config);
+    if (settings) $.extend(config, settings);  // replace defaults with input arguments
+
 
 
     /* Public methods
@@ -29,9 +34,9 @@ function CollectionList(preview_fnc, select_fnc) {
     this.getFullCollectionList = function(filter, outputelement) {
 
         outputelement = ((typeof outputelement !== 'undefined') ? outputelement : false);
-        filter = ((typeof filter !== 'undefined') ? '&'+filter : '');
+        filter = ((typeof filter !== 'undefined') ? '?'+filter : '');
 
-        var list = createCollectionList(api_baseurl+filter);
+        var list = createCollectionList(config.api_baseurl+filter);
 
         if (outputelement) {
             $(outputelement).html(list);
@@ -51,9 +56,9 @@ function CollectionList(preview_fnc, select_fnc) {
     this.getWorksetList = function(filter, parent_id, outputelement) {
 
         outputelement = ((typeof outputelement !== 'undefined') ? outputelement : false);
-        filter = ((typeof filter !== 'undefined') ? '&'+filter : '');
+        filter = ((typeof filter !== 'undefined') ? '?'+filter : '');
 
-        var list = createCollectionList(api_baseurl+filter, parent_id);
+        var list = createCollectionList(config.api_baseurl+filter, parent_id);
 
         if (outputelement) {
             $(outputelement).html(list);
@@ -134,39 +139,57 @@ function CollectionList(preview_fnc, select_fnc) {
         var link = '';
         var actions = '';
         var sublist = '';
+        var itemtype = '';
         var actionclass = 'btn '; // btn-small
+        var infoclass = 'btn btn-mini disabled ';
 
         if ( !parent_id ) { // This item is a parent Collection
-            link = linkurl+clobj.id+'/';
+            link = config.linkurl+clobj.id+'/';
             actions += '<li class="nav-header">Jump to:</li><li><a href="'+link+'#map" title="View Workset map"><i class="icon-globe"></i> Map view</a></li>';
             actions += '<li><a href="'+link+'#thm" title="View Workset images"><i class="icon-picture"></i> Thumbnail view</a></li>';
             actions += '<li class="nav-header">Data Tasks:</li><li><a href="'+link+'#NewWorksetModal" title="Create new Workset"><i class="icon-plus"></i> Create new Workset</a></li>';
             actions += '<li><a href="'+link+'#dwn" title="Download Data"><i class="icon-download-alt"></i> Download</a></li>';
-            actionclass += 'btn-primary';
+
+            actionclass += 'btn-primary ';
+            itemtype = 'Collection';
 
             sublist += createCollectionList(api_url, clobj.id); // Recursive call to create nested workset list (if available)
 
         } else { // This item is a child Collection (i.e: a Workset)
-            link = linkurl+parent_id+'/'+clobj.id + '/';
+            link = config.linkurl+parent_id+'/'+clobj.id + '/';
             actions += '<li class="nav-header">Jump to:</li><li><a href="'+link+'#map" title="View Collection map"><i class="icon-globe"></i> Map view</a></li>';
             actions += '<li><a href="'+link+'#thm" title="View Collection images"><i class="icon-picture"></i> Thumbnail view</a></li>';
             actions += '<li class="nav-header">Data Tasks</li><li><a href="/imageview" class="imageframe" data-fancybox-group="group'+clobj.id+'" data-fancybox-type="iframe" title="Annotate Workset"><i class="icon-tag"></i> Annotate Workset</a></li>';
             actions += '<li><a href="'+link+'#dwn" title="Download Data"><i class="icon-download-alt"></i> Download</a></li>';
 
+            itemtype = 'Workset';
+
             //actionclass += 'btn-mini';
         }
 
         var listitem = '<li>';
-        listitem += '<span class="clname list-toggle">' + clobj.name + ' ('+clobj.creation_info+')</span> ';
-        listitem += '<span class="clactions btn-group">';
-        if (preview_fnc) listitem += '<button class="'+actionclass+' list-toggle" onclick="'+preview_fnc+'('+clobj.id+');" rel="btn-tooltip" title="Preview"><i class="icon-eye-open"></i></button>';
-        listitem += '<a class="'+actionclass+'" href="'+link+'" rel="btn-tooltip" title="Select"><i class="icon-external-link"></i></a>';
+        listitem += '<span class="btn-toolbar clactions">';
+        listitem += '<span class="btn-group">';
+        listitem += '<span class="cldate '+infoclass+'"  rel="btn-tooltip" title="This item was created on '+clobj.creation_date+'">'+clobj.creation_date.substr(0,10)+'</span> ';
+        listitem += '<span class="clowner '+infoclass+'"  rel="btn-tooltip" title="This item is owned by '+clobj.owner.username+'">'+clobj.owner.username+'</span> ';
+        if (clobj.is_public) {
+            listitem += '<span class="claccess '+infoclass+' btn-danger" rel="btn-tooltip" title="This item is publicly accessible">Public</span> ';
+        } else {
+            listitem += '<span class="claccess '+infoclass+' btn-success" rel="btn-tooltip" title="This item is private">Private</span> ';
+        }
+        listitem += '</span> ';
+        listitem += '<span class="btn-group">';
+        if (config.preview_fnc) listitem += '<button class="'+actionclass+'" onclick="'+config.preview_fnc+'('+clobj.id+');" rel="btn-tooltip" title="Preview"><i class="icon-eye-open"></i></button>';
+        if (config.select_fnc) {
+            listitem += '<button class="'+actionclass+'" onclick="'+config.select_fnc+'('+clobj.id+');" rel="btn-tooltip" title="Select"><i class="icon-external-link"></i></button>';
+        } else {
+            listitem += '<a class="'+actionclass+'" href="'+link+'" rel="btn-tooltip" title="Select"><i class="icon-external-link"></i></a>';
+        }
         listitem += '<button class="'+actionclass+' dropdown-toggle" data-toggle="dropdown" rel="btn-tooltip" title="More..."><b class="caret"></b></button>';
         listitem += '<ul class="dropdown-menu">'+actions+'</ul>';
-        listitem += '</span> ';
-        listitem += '<span class="cldate"><br>'+(clobj.creation_date)+'</span> ';
-        listitem += '<span class="clowner">'+clobj.owner.username+'</span> ';
-        listitem += '<span class="claccess">'+(clobj.is_public ? 'Public': 'Private') +'</span> ';
+        listitem += '</span></span>';
+        listitem += '<span class="clname list-toggle" >'+itemtype+': ' + clobj.name + ' ('+clobj.creation_info+')</span> ';
+        //listitem += '<span class="cldate">'+clobj.creation_date.substr(0,10)+'</span> ';
         if (clobj.description) listitem += '<span class="cldescription"><br>'+clobj.description+'</span>'
         listitem += sublist;
         listitem += '</li>';
@@ -176,3 +199,40 @@ function CollectionList(preview_fnc, select_fnc) {
     }
 }
 
+
+function CollectionInfo(settings) {
+    // Set default config params
+    var config = jQuery.extend({},collection_global_config);
+    if (settings) $.extend(config, settings);  // replace defaults with input arguments
+
+
+
+    this.getCollectionInfo = function(id,outputelement) {
+        nolinks = ((typeof nolinks !== 'undefined') ? nolinks : true);
+        var api_url = config.api_baseurl+'?id='+id;
+        var clinfo = '';
+        $.ajax({
+            dataType: "json",
+            async: false,  // prevent asyncronous mode to allow setting of variables within function
+            url: api_url ,
+            success: function(cl){
+                var clobj = cl.objects[0];
+                var link = ((clobj.parent == 'none') ? config.linkurl+clobj.id+'/' : link = config.linkurl+clobj.parent+'/'+clobj.id + '/');
+
+                clinfo += '<span class="clactions btn-group">';
+                if (config.preview_fnc) clinfo += '<button class="btn" onclick="'+config.select_fnc+'('+clobj.id+');" rel="btn-tooltip" title="Preview"><i class="icon-eye-open"></i></button>';
+                if (config.select_fnc) clinfo += '<button class="btn" onclick="'+config.preview_fnc+'('+clobj.id+');" rel="btn-tooltip" title="Select"><i class="icon-external-link"></i></button>';
+                clinfo += '</span> ';
+                clinfo += '<span class="clname">' + clobj.name + ' ('+clobj.creation_info+')</span> ';
+                clinfo += '<span class="cldate">'+(clobj.creation_date)+'</span> ';
+                clinfo += '<span class="clowner">'+clobj.owner.username+'</span> ';
+                clinfo += '<span class="claccess">'+(clobj.is_public ? 'Public': 'Private') +'</span> ';
+                if (clobj.description) clinfo += '<span class="cldescription">'+clobj.description+'</span>'
+            }
+        })
+        if (outputelement) {
+            $(outputelement).html(clinfo);
+        }
+        return clinfo;
+    }
+}
--- webinterface/static/assets/catami-api/js/filters.js
+++ webinterface/static/assets/catami-api/js/filters.js
@@ -1,9 +1,48 @@
-function ApiFilters(updatefnc) {
+/**
+ * Class for creating API filter string from input elements.
+ *
+ * An example filter could be:
+ *
+    <div class="btn-group api-filter">
+        <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">Parameter name: <span class="api-filt-text">Default text</span> <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+            <li><label class="radio"><input type="radio" name="filtparam" value="VALUE1" checked>Option 1</label></li>
+            <li><label class="radio"><input type="radio" name="filtparam" value="VALUE2">Option 2</label></li>
+        </ul>
+    </div>
+ *
+ * NOTE: the class of the div: "api-filter"
+ *       the class of the span: "api-filt-text"
+ *       the name attribute of the input "filtparam" is the name of the api filter parameter
+ *       the value attribute of the input is the value of the api filter parameter
+ *       the label and the input is used to set the content "api-filt-text" span
+ *
+ * In JS, you would initialise with:
+ *
+    filt=new ApiFilter(function {  // constructor with onChange method
+        update_filter();
+    });
+    filt.init;                      // initialise
+    filt.update;                    // initial update using defaults (optional)
+
+    function update_filter() {      // function called when filter is updated
+        filterstring = filt.get;
+        // Do stuff with filterstring
+    }
+ *
+ * @param updatefnc
+ * @constructor
+ */
+
+
+function ApiFilter(updatefnc) {
 
     this.update = updatefnc;
 
-    // Searches for all radio buttons contained within the "api-filter" class and
-    // sets change functions for filter radio buttons
+    /**
+     * Searches for all radio buttons contained within the "api-filter" class and
+     * sets change functions for filter radio buttons
+     */
     this.init = function() {
         $('.api-filter input:radio').on('change',function() {
             updatefnc();
@@ -11,6 +50,12 @@ function ApiFilters(updatefnc) {
     }
 
 
+    /**
+     * Searches for input elements contained within in the "api-filter" class and
+     * uses their names and values to construct a filter string.
+     * It also updates the html of the nearest parent element containing the "api-filt-text" class label
+     * @return {String}
+     */
     this.get = function() {
         var filter = 'format=json';
 
