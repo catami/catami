9f7c16fedd7f0f7330f6107936793dd4555b3419
Lachlan Toohey <l.toohey@acfr.usyd.edu.au>
2013-04-12 16:40:43 +1000

    Purge much of staging that is no longer to be used.

--- staging/forms.py
+++ staging/forms.py
@@ -4,19 +4,11 @@ This includes AUVImportForm and FileImportForm.
 """
 from django import forms
 
-from django.contrib.gis.forms import fields as gisfields
-from django.db import models
-from catamidb.models import Campaign, Deployment
-
-from .models import MetadataFile
-
-from .widgets import PointField, MultiSourceField
-from .fields import MultiFileField
+from catamidb.models import Campaign
 
 import logging
 
 logger = logging.getLogger(__name__)
-
 
 class CampaignCreateForm(forms.ModelForm):
     class Meta:
@@ -32,85 +24,25 @@ class FileImportForm(forms.Form):
 
     upload_file.help_text = "JSON file in import format."
 
-
-class MetadataStagingForm(forms.ModelForm):
-    """Form to upload generic files that will need processing.
-    """
-
-    class Meta:
-        model = MetadataFile
-        exclude = ('owner', )
-
-
-class ModelImportForm(forms.Form):
-    """Form to handle general importing of data to deployments.
-
-    This takes data from existing structures and helps create
-    a mapping.
-
-    Internally it mimics a lot of code from django/forms/models.py
-    with regards to model introspection to get a list of fields.
-    """
-
-    def __init__(self, *args, **kwargs):
-        """Create the ModelImportForm from a model.
-
-        This creates all the fields 'on the fly' from the model enabling
-        it to adapt to any models given to it.
-        """
-        columns = kwargs.pop('columns')
-        model = kwargs.pop('model')
-
-        logger.debug("ModelImportForm init'ing parent")
-
-        # init the form
-        super(ModelImportForm, self).__init__(*args, **kwargs)
-
-        # get the model fields (the dbmodel)
-        # and add the form fields to match
-        if issubclass(model, models.Model):
-            # get the fields
-            logger.debug(
-                "ModelImportForm getting all fields of {0}".format(model))
-            all_model_fields = model._meta.fields
-            # this is a list of fields
-            # each field has:
-            # - name
-            # - model (class of actual model it is declared in)
-            # - formfield() constructs a matching form.field
-
-            for model_field in all_model_fields:
-                # create the default form field for that type
-                form_field = model_field.formfield(required=False)
-                logger.debug(
-                    'ModelImportForm model field {0} has form field {1}'.format(
-                        model_field, form_field))
-                if form_field:
-                    # as long as there is a corresponding form field
-                    # (autofield - pk does not have one)
-
-                    # make a split date time, or use lat lon/not the geometry
-                    # field
-                    if type(form_field) == forms.DateTimeField:
-                        form_field = forms.SplitDateTimeField(required=False)
-                    elif type(form_field) == gisfields.GeometryField:
-                        form_field = PointField(required=False)
-
-                    # a little bit improper but required=False prevents it
-                    # check all the sub fields. The compress phase does
-                    # actually check that it has a valid value
-                    # and ignores this required flag against convention.
-                    self.fields[model_field.name] = MultiSourceField(
-                        base_field=form_field, columns=columns,
-                        required=False)
-
-        else:
-            # wrong type of class
-            raise TypeError("Expected subclass of django.db.models.Model")
-
-
 class ApiDeploymentForm(forms.Form):
     short_name = forms.CharField()
     campaign = forms.ModelChoiceField(queryset=Campaign.objects.all())
     license = forms.CharField(max_length=500)
     descriptive_keywords = forms.CharField(max_length=500)
+
+    def clean_short_name(self):
+        """Perform extra cleaning on the field.
+
+        In this case strip non valid characters from the name. Mainly
+        the slash, and backslash.
+        """
+
+        # get the existing value
+        sn = self.cleaned_data['short_name']
+
+        # remove the characters
+        sn = sn.translate(None, "\\/")
+
+        # and return the newly recleaned value
+        return sn
+
--- staging/urls.py
+++ staging/urls.py
@@ -7,51 +7,12 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('staging.views',
                        url(r'^$', 'index', name='staging_index'),
-                       #url(r'^accounts/'+str(User.get), '---', name='user_url'),
-                       url(r'^upload_progress/$', 'upload_progress'),
 
                        # campaign creating
                        url(r'^campaign/create$', 'campaigncreate',
                            name='staging_campaign_create'),
                        url(r'^campaign/created$', 'campaigncreated',
                            name='staging_campaign_created'),
-
-                       # django json deserialisation
-                       url(r'^file/import$', 'fileupload',
-                           name='staging_file_import'),
-                       url(r'^file/imported$', 'fileuploaded',
-                           name='staging_file_imported'),
-
-                       # model metadata loading
-                       url(r'^metadata/stage$', 'metadatastage',
-                           name='staging_metadata_stage'), # upload/import file
-                       url(r'^metadata/list$', 'metadatalist',
-                           name='staging_metadata_list'), # list files
-                       url(r'^metadata/change_public/$', 'change_public',
-                           name='staging_metadata_book_update_public'),
-                       # list sheets and headings
-                       url(r'^metadata/book/(?P<file_id>\d+)$', 'metadatabook',
-                           name='staging_metadata_book'),
-                       # list sheets and headings
-                       url(r'^metadata/book/(?P<file_id>\d+)/delete$',
-                           'metadatadelete',
-                           name='staging_metadata_delete'), # delete a file
-                       url(
-                           r'^metadata/sheet/(?P<file_id>\d+)/(?P<page_name>[\w-]+)$',
-                           'metadatasheet',
-                           name='staging_metadata_sheet'),
-                       # show sheet data/info
-                       url(
-                           r'^metadata/import/(?P<file_id>\d+)/(?P<page_name>[\w-]+)/(?P<model_name>[\w-]+)$',
-                           'metadataimport', name='staging_metadata_import'),
-                       # show import data/info
-                       url(r'^metadata/imported$', 'metadataimported',
-                           name='staging_metadata_imported'),
-                       # thanks/confirmation of import
-
-                       # annotations
-                       #url(r'^annotations/cpc/import$', 'annotationcpcimport', name='staging_annotation_cpc_import'),
-                       #url(r'^annotations/cpc/imported$', 'annotationcpcimported', name='staging_annotation_cpc_imported'),
 
                        url(r'^browse$', 'newbrowse'),
                        url(r'^create/auv/(?P<pk>\w[\w-]*)$', 'api_auv_form',
