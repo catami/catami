5f7814fc1d157107f522907008924b57a428b26d
Mat <mathew.wyatt@gmail.com>
2013-04-17 13:54:04 +0800

    Packed map code into single js file

--- webinterface/static/assets/catami-api/js/Map.js
+++ webinterface/static/assets/catami-api/js/Map.js
@@ -178,4 +178,172 @@ BaseMap.prototype.clearMap = function() {
         this.mapInstance.removeLayer(this.mapInstance.getLayersByName("Images")[0]);
 }
 
+/**
+ *
+ * Openlayers map for the projects page
+ *
+ * @param wmsUrl
+ * @param wmsLayerName
+ * @param divName
+ * @constructor
+ * @param extentUrl - this is the URL in which to query the extent of a collection
+ */
+function ProjectsMap(wmsUrl, wmsLayerName, divName, extentUrl) {
+    this.BaseMap = BaseMap;
+    this.BaseMap(wmsUrl, wmsLayerName, divName);
+    this.extentUrl = extentUrl;
+}
+
+/**
+ * Extend from the BaseMap
+ * @type {BaseMap}
+ */
+ProjectsMap.prototype = new BaseMap;
+
+/**
+ *
+ * Will take a collectionId and update the map
+ *
+ * @param collectionId
+ */
+ProjectsMap.prototype.updateMapForSelectedCollection = function(collectionId) {
+    var filter_array = [];
+
+    filter_array.push(new OpenLayers.Filter.Comparison({
+        type: OpenLayers.Filter.Comparison.EQUAL_TO,
+        property: "collection_id",
+        value: collectionId
+    }));
+
+    this.updateMapUsingFilter(filter_array);
+};
+
+/**
+ * Will update the bounds of a collection given it's id
+ */
+ProjectsMap.prototype.updateMapBoundsForCollection = function(collectionId) {
+    var mapInstance = this.mapInstance;
+    $.ajax({
+        type: "POST",
+        url: this.extentUrl,
+        data: "collection_id="+collectionId,
+        success: function (response, textStatus, jqXHR) {
+            var boundsArr = response.extent.replace("(", "").replace(")", "").split(",");
+            var bounds = new OpenLayers.Bounds();
+            bounds.extend(new OpenLayers.LonLat(boundsArr[0], boundsArr[1]));
+            bounds.extend(new OpenLayers.LonLat(boundsArr[2], boundsArr[3]));
+            var geographic = new OpenLayers.Projection("EPSG:4326");
+            var mercator = new OpenLayers.Projection("EPSG:900913");
+            mapInstance.zoomToExtent(bounds.transform(geographic, mercator));
+        }
+    });
+};
+
+/**
+ *
+ * Openlayers map for the explore page
+ *
+ * @param wmsUrl
+ * @param wmsLayerName
+ * @param divName
+ * @constructor
+ */
+function ExploreMap(wmsUrl, wmsLayerName, divName, extentUrl) {
+    this.BaseMap = BaseMap;
+    this.BaseMap(wmsUrl, wmsLayerName, divName);
+    this.extentUrl = extentUrl;
+}
+
+ExploreMap.prototype = new BaseMap;
+
+/**
+ *
+ * Used for constructing the filter in which to run on the WMS
+ *
+ * @param minDepth
+ * @param maxDepth
+ * @param minAltitude
+ * @param maxAltitude
+ * @param minTemperature
+ * @param maxTemperature
+ * @param minSalinity
+ * @param maxSalinity
+ * @returns {Array}
+ */
+ExploreMap.prototype.createExploreFilterArray = function(minDepth,
+                                                         maxDepth,
+                                                         minAltitude,
+                                                         maxAltitude,
+                                                         minTemperature,
+                                                         maxTemperature,
+                                                         minSalinity,
+                                                         maxSalinity,
+                                                         deploymentIds) {
+
+    var filter = [
+        new OpenLayers.Filter.Comparison({
+            type: OpenLayers.Filter.Comparison.BETWEEN,
+            property: "depth",
+            lowerBoundary: minDepth,
+            upperBoundary: maxDepth
+        }),
+        new OpenLayers.Filter.Comparison({
+            type: OpenLayers.Filter.Comparison.BETWEEN,
+            property: "altitude",
+            lowerBoundary: minAltitude,
+            upperBoundary: maxAltitude
+        }),
+        new OpenLayers.Filter.Comparison({
+            type: OpenLayers.Filter.Comparison.BETWEEN,
+            property: "temperature",
+            lowerBoundary: minTemperature,
+            upperBoundary: maxTemperature
+        }),
+        new OpenLayers.Filter.Comparison({
+            type: OpenLayers.Filter.Comparison.BETWEEN,
+            property: "salinity",
+            lowerBoundary: minSalinity,
+            upperBoundary: maxSalinity
+        })
+    ];
+
+    var deploymentIdFilter = [];
+    for (var i = 0; i < deploymentIds.length; i++) {
+        deploymentIdFilter.push(new OpenLayers.Filter.Comparison({
+            type: OpenLayers.Filter.Comparison.EQUAL_TO,
+            property: "deployment_id",
+            value: deploymentIds[i]
+        }));
+    }
+
+    deploymentIdFilter = new OpenLayers.Filter.Logical({
+        type: OpenLayers.Filter.Logical.OR,
+        filters: deploymentIdFilter
+    });
+
+    filter.push(deploymentIdFilter);
+
+    return filter;
+};
+
+ExploreMap.prototype.updateMapBoundsForDeployments = function(deploymentIds) {
+    var mapInstance = this.mapInstance;
+    $.ajax({
+        type: "POST",
+        url: this.extentUrl,
+        data: "deployment_ids=" + currentSelectedDeployments,
+        success: function (response, textStatus, jqXHR) {
+            var boundsArr = response.extent.replace("(", "").replace(")", "").split(",");
+            var bounds = new OpenLayers.Bounds();
+            bounds.extend(new OpenLayers.LonLat(boundsArr[0], boundsArr[1]));
+            bounds.extend(new OpenLayers.LonLat(boundsArr[2], boundsArr[3]));
+            var geographic = new OpenLayers.Projection("EPSG:4326");
+            var mercator = new OpenLayers.Projection("EPSG:900913");
+            mapInstance.zoomToExtent(bounds.transform(geographic, mercator));
+        }
+    });
+};
+
+
+
 
