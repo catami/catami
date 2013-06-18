b258d22ec1741137943dd2d6f6e3410172d0e449
Mark Gray <markg@ivec.org>
2013-06-18 23:03:41 +0800

    ImageUploadResource create_obj fixed for image file and thumbnail creation.

--- catamidb/api.py
+++ catamidb/api.py
@@ -21,7 +21,7 @@ from .models import *
 from restthumbnails.helpers import get_thumbnail_proxy
 from catamidb import authorization
 
-import os, sys
+import os, sys, shutil
 import PIL
 
 import logging
@@ -825,6 +825,7 @@ class ImageResource(ModelResource):
 
         return images
 
+
 class ImageUploadResource(BackboneCompatibleResource):
     img = fields.FileField(attribute="img", null=True, blank=True)
 
@@ -838,7 +839,7 @@ class ImageUploadResource(BackboneCompatibleResource):
         filtering = {
             'collection': ALL,
         }
-        allowed_methods = ['get', 'post'] #allow post to create campaign via Backbonejs
+        allowed_methods = ['get', 'post']  # allow post to create campaign via Backbonejs
 
     def deserialize(self, request, data, format=None):
         if not format:
@@ -852,53 +853,74 @@ class ImageUploadResource(BackboneCompatibleResource):
         return super(ImageUploadResource, self).deserialize(request, data, format)
 
     def obj_create(self, bundle, **kwargs):
-                  
-        if ("img" in bundle.data.keys() and 
-            "deployment" in bundle.data.keys()):
+        if ("img" in bundle.data.keys() and "deployment" in bundle.data.keys()):
             #Split image name and image path
-            sourcePath, imgName =  os.path.split(str(bundle.data["img"]))
+            sourcePath, imgName = os.path.split(str(bundle.data["img"]))
             if imgName.find("@") != -1:
-                imgName = imgName[:1] #remove "@" if exists
-            #split image name and image extension    
+                imgName = imgName[:1]  # remove "@" if exists
+
+            # split image name and image extension
+
             imgNameNoExt, imgExt = os.path.splitext(imgName)
-            
-            if (imgExt.lower() == ".png" or
-                imgExt.lower() == ".jpg" or
-                imgExt.lower() == ".jpeg"):
-                
+
+            if (imgExt.lower() == ".png" or imgExt.lower() == ".jpg" or imgExt.lower() == ".jpeg"):
+
                 deployment = self.Meta.deployments.filter(id=int(bundle.data["deployment"]))
-                #Use specified deployment id to get Campaign and Deployment names, which is used to create path for image and thumbnails
+
+                # Use specified deployment id to get Campaign and Deployment names, which is used to create path for image and thumbnails
+
                 if deployment.exists():
                     deploymentName = deployment[0].short_name
-                    campaignName = deployment[0].campaign.short_name                
-                    imageDest = os.path.join(settings.IMPORT_PATH, campaignName, deploymentName, "images","")
-                    thumbDest = os.path.join(settings.IMPORT_PATH, campaignName, deploymentName, "thumbnails","")
-                    bundle.obj.img.field.upload_to = imageDest
-                    
+                    campaignName = deployment[0].campaign.short_name
+
+                    # django does like us to write files outside MEDIA_ROOT using djanjo functions,
+                    # so we create the image in MEDIA_ROOT and then move it to the desired location (deloymeent directory)
+
+                    temp_dir = os.path.join(settings.MEDIA_ROOT, 'import_temp')
+
+                    if not os.path.exists(temp_dir):
+                        os.makedirs(temp_dir)
+
+                    imageDest = os.path.join(settings.IMPORT_PATH, campaignName, deploymentName, "images", "")
+                    thumbDest = os.path.join(settings.IMPORT_PATH, campaignName, deploymentName, "thumbnails", "")
+                    bundle.obj.img.field.upload_to = temp_dir
+
+                    if not os.path.exists(imageDest):
+                        logger.debug("Created directory for images: %s" % imageDest)
+                        os.makedirs(imageDest)
+
                     super(ImageUploadResource, self).obj_create(bundle, **kwargs)
-                    
+
+                    shutil.move(os.path.join(temp_dir, imgName), imageDest)
+
                     logger.debug("%s uploaded to server.." % imgName)
                     size = str(settings.THUMBNAIL_SIZE[0]) + "x" + str(settings.THUMBNAIL_SIZE[1])
-                    
+
                     infile = os.path.normpath(imageDest + imgName)
-                    outfile = os.path.normpath(thumbDest + imgNameNoExt+ "_" + size + imgExt)
-                    
-                    if infile != outfile:
-                        try:
-                            if not os.path.exists(thumbDest):
-                                os.makedirs(thumbDest)
-                                im = PIL.Image.open(infile)
-                                im.thumbnail(settings.THUMBNAIL_SIZE, PIL.Image.ANTIALIAS)                       
-                                im.save(outfile, "JPEG")
-                                logger.debug("Thumbnail imagery %s created." % outfile)
-                        except IOError as io:
-                            logger.debug("cannot create thumbnail for '%s'" % infile)
+                    outfile = os.path.normpath(thumbDest + imgNameNoExt + "_" + size + imgExt)
+
+                    logger.debug("imageDest %s" % imageDest)
+                    logger.debug("thumbDest %s" % thumbDest)
+                    logger.debug("deploymentName %s" % deploymentName)
+                    logger.debug("infile %s" % infile)
+                    logger.debug("outfile %s" % outfile)
+
+                    try:
+                        if not os.path.exists(thumbDest):
+                            os.makedirs(thumbDest)
+                        im = PIL.Image.open(infile)
+                        im.thumbnail(settings.THUMBNAIL_SIZE, PIL.Image.ANTIALIAS)
+                        im.save(outfile, "JPEG")
+                        logger.debug("Thumbnail imagery %s created." % outfile)
+                    except IOError:
+                        logger.debug("cannot create thumbnail for '%s'" % infile)
+
                 else:
-                    raise ImmediateHttpResponse(response=http.HttpBadRequest("Invalid Deployment ID specified:"+str(bundle.data["deployment"])))    
+                    raise ImmediateHttpResponse(response=http.HttpBadRequest("Invalid Deployment ID specified:"+str(bundle.data["deployment"])))
             else:
-                    raise ImmediateHttpResponse(response=http.HttpBadRequest("Image format not supported! Supported images include .png, .jpg, .jpeg"))  
+                    raise ImmediateHttpResponse(response=http.HttpBadRequest("Image format not supported! Supported images include .png, .jpg, .jpeg"))
         else:
-            raise ImmediateHttpResponse(response=http.HttpBadRequest("Please specify 'img', 'deployment' and 'thumbnailsize' parameters"))    
+            raise ImmediateHttpResponse(response=http.HttpBadRequest("Please specify 'img', 'deployment' and 'thumbnailsize' parameters"))
         return bundle
 
 
