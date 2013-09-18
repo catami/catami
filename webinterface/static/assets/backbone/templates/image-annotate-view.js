4101bc9ea5f2f878c8f98c1885e4273e5b3f0097
Mark Gray <markg@ivec.org>
2013-09-18 10:40:54 +0800

    Separated image-annotate-view.js

---
+++ webinterface/static/assets/backbone/templates/image-annotate-view.js
@@ -0,0 +1,394 @@
+ImageAnnotateView = Backbone.View.extend({
+    model: new PointAnnotation(),
+    el: $('div'),
+    events: {
+        "thumbnail_selected": "thumbnailSelected"
+    },
+    initialize: function () {
+        var parent = this;
+
+        //bind to the global event, so we can get events from other views
+        GlobalEvent.on("thumbnail_selected", this.thumbnailSelected, this);
+        GlobalEvent.on("thumbnail_selected_by_id", this.thumbnailSelectedWithId, this);
+        GlobalEvent.on("screen_changed", this.screenChanged, this);
+        GlobalEvent.on("point_clicked", this.pointClicked, this);
+        GlobalEvent.on("point_mouseover", this.pointMouseOver, this);
+        GlobalEvent.on("point_mouseout", this.pointMouseOut, this);
+        GlobalEvent.on("annotation_to_be_set", this.annotationChosen, this);
+        GlobalEvent.on("refresh_point_labels_for_image", this.refreshPointLabelsForImage, this);
+        GlobalEvent.on("hide_points", this.hidePoints, this);
+        GlobalEvent.on("show_points", this.showPoints, this);
+        GlobalEvent.on("deselect_points", this.deselectPoints, this);
+    },
+    renderSelectedImageById: function (id) {
+
+        //get all the images to be rendered
+        var imageTemplate = "";
+        var imageVariables = {
+            "web_location": $('#' + id).data("web_location")
+        };
+
+        imageTemplate += _.template($("#ImageTemplate").html(), imageVariables);
+
+        // Load the compiled HTML into the Backbone "el"
+        this.$el.html(imageTemplate);
+        $('.FineScaleEditBox').show();
+
+        return this;
+    },
+    renderSelectedImage: function (selected) {
+        //get all the images to be rendered
+        var imageTemplate = "";
+
+        // enforcing only one annotation set per project for the time being, so
+        // can assume the first one
+        var annotationSet = annotationSets.at(0);
+        var image = annotationSet.get("images")[selected];
+
+        var imageVariables = {
+            "web_location": image.web_location
+        };
+        imageTemplate += _.template($("#ImageTemplate").html(), imageVariables);
+
+        // Load the compiled HTML into the Backbone "el"
+        this.$el.html(imageTemplate);
+
+        return this;
+    },
+    refreshPointLabelsForImage: function() {
+        //checks annotated point list in view.  If any label has blank label text, then
+        //get it from the relevant API call.
+        //
+        //labels are made blank when a previouslt labeled point is once again selected
+
+        var annotatedPoints = $('.pointAnnotated');
+
+        $.each(annotatedPoints, function(index, pointSpan) {
+            // refresh annotated
+            if ($(pointSpan).text() === ""){
+                var newpoint = new PointAnnotation({id: pointSpan.id});
+                newpoint.fetch({success:function(model) {
+                    var annotation_object = annotationCodeList.find(function(listmodel) {
+                        return listmodel.get('caab_code')===newpoint.get('annotation_caab_code');
+                    });
+                    $(pointSpan).text(annotation_object.get("cpc_code"));
+                }});
+            }
+        });
+    },
+    renderPointsForImage: function() {
+
+        //destroy all the tooltips before loading new points
+        this.pointMouseOut();
+
+        var parent = this;
+
+        //get the selected image
+        var annotationSet = annotationSets.at(0);
+        //var image = annotationSet.get("images")[selected];
+        
+        this.disableAnnotationSelector();
+
+        //query the API for the points for the current image
+
+        points.fetch({
+            data: { limit: 100, image: selectedImageId, annotation_set: annotationSet.get('id') },
+            success: function (model, response, options) {
+
+                //loop through the points and apply them to the image
+                points.each(function (point) {
+                    var pointId = point.get('id');
+                    var label = point.get('annotation_caab_code');
+
+                    var annotationCode = annotationCodeList.find(function(model) {
+                        return model.get('caab_code')===point.get('annotation_caab_code');
+                    });
+
+                    var labelClass = (label === "") ? 'pointNotAnnotated' : 'pointAnnotated';
+
+                    var span = $('<span>');
+                    span.attr('id', pointId);
+                    span.attr('class', labelClass);
+                    span.css('top', point.get('y')*$('#Image').height()-6);
+                    span.css('left', point.get('x')*$('#Image').width()-6) ;
+                    span.css('z-index', 10000);
+                    span.attr('caab_code', label);
+                    span.attr('rel', 'tooltip');
+                    span.attr('data-container','#ImageAppContainer');
+
+                    if (labelClass === 'pointAnnotated'){
+                        span.text(annotationCode.get("cpc_code"));
+                        span.attr('title', annotationCode.get("code_name"));
+                    }
+
+                    span.appendTo('#ImageContainer');
+                    span.tooltip();
+                });
+
+                $("[rel=tooltip]").tooltip();
+
+                $("#ImageContainer").children('span').click(function(){
+                    GlobalEvent.trigger("point_clicked", this);
+                });
+
+                $("#ImageContainer").children('span').mouseover(function(){
+                    GlobalEvent.trigger("point_mouseover", this);
+                });
+
+                $("#ImageContainer").children('span').mouseout(function(){
+                    GlobalEvent.trigger("point_mouseout", this);
+                });
+
+                //update pils
+                GlobalEvent.trigger("image_points_updated", this);
+
+            },
+            error: function (model, response, options) {
+                $.pnotify({
+                    title: 'Failed to load the points for this image. Try refreshing the page.',
+                    text: response.status,
+                    type: 'error', // success | info | error
+                    hide: true,
+                    icon: false,
+                    history: false,
+                    sticker: false
+                });
+            }
+        });
+
+        $("[rel=tooltip]").tooltip();
+
+    },
+    thumbnailSelectedWithId: function(ImageId) {
+
+        //destroy the tooltips
+        this.pointMouseOut();
+
+        this.renderSelectedImageById(ImageId);
+
+        var parent = this;
+        //now we have to wait for the image to load before we can draw points
+        $("#Image").imagesLoaded(function() {
+            parent.renderPointsForImage();
+        });
+
+    },
+    thumbnailSelected: function(selectedPosition) {
+
+        //destroy the tooltips
+        this.pointMouseOut();
+
+        selectedThumbnailPosition = selectedPosition;
+        this.renderSelectedImage(selectedPosition);
+
+        //turn the zoom off and reset the zoom button
+        //this.zoomOff();
+        //$('#ZoomToggle').removeClass('active');
+
+        var parent = this;
+        //now we have to wait for the image to load before we can draw points
+        $("#Image").imagesLoaded(function() {
+            parent.renderPointsForImage(selectedPosition);
+        });
+
+    },
+    screenChanged: function() {
+        //loop through the points and apply them to the image
+        points.each(function (point) {
+            var pointId = point.get('id');
+            var span = $('#'+pointId);
+
+            span.css('top', point.get('y')*$('#Image').height());
+            span.css('left', point.get('x')*$('#Image').width());
+        });
+    },
+    pointClicked: function(thePoint) {
+        var theClass = $(thePoint).attr('class');
+        var theCaabCode = $(thePoint).attr('caab_code');
+    
+
+        this.enableAnnotationSelector();
+    
+        if(theClass == 'pointSelected' && theCaabCode == ""){
+            $(thePoint).attr('class', 'pointNotAnnotated');
+        } else if(theClass == 'pointSelected' && theCaabCode != ""){
+            $(thePoint).attr('class', 'pointAnnotated');
+        } else {
+
+            //firstly we need to check if we need to deselect already labelled points
+            $(".pointLabelledStillSelected").each(function (index, pointSpan) {
+                $(pointSpan).attr('class', 'pointAnnotated');
+            });
+
+            //then we make the current points selected
+            $(thePoint).attr('class', 'pointSelected');
+            //hide the label, if there is one
+            $(thePoint).text("");
+            GlobalEvent.trigger("point_is_selected", this);
+        }
+
+        this.refreshPointLabelsForImage();
+    },
+    pointMouseOver: function(thePoint) {
+        //get points which have the same caab code assigned
+        var samePoints = points.filter(
+            function(point) {
+                return point.get("annotation_caab_code") == $(thePoint).attr('caab_code');
+            }
+        );
+
+        //show the labels
+        for(var i = 0; i < samePoints.length; i++) {
+            $("#"+samePoints[i].get("id")).tooltip('show');
+        }
+    },
+    pointMouseOut: function() {
+        //remove labels from all points
+        points.each(function(point) {
+            $("#"+point.get("id")).tooltip('destroy');
+        });
+    },
+    annotationChosen: function(caab_code_id) {
+        var parent = this;
+
+        //get the selected points
+        var selectedPoints = $('.pointSelected');
+        caab_object = annotationCodeList.get(caab_code_id);
+
+        //only need a callback function if points greater than 0
+        var afterAllSavedCallback;
+        if(selectedPoints.length > 0)
+            afterAllSavedCallback = _.after(selectedPoints.length, function() {
+                //send out an event for all the other listeners
+                GlobalEvent.trigger("annotation_set_has_changed");
+            });
+
+        //save the annotations
+        $.each(selectedPoints, function(index, pointSpan) {
+
+
+            //need to specify the properties to patch
+            var properties = { 'annotation_caab_code': caab_object.get('caab_code') };
+
+            var theXHR = points.get(pointSpan.id).save(properties, {
+                patch: true,
+                headers: {"cache-control": "no-cache"},
+                success: function (model, xhr, options) {
+
+                    //show label on annotated point
+                    var idOfSaved = model.get("id");
+                    $('#'+idOfSaved).addClass('pointLabelledStillSelected'); //this means the point stays selected, we are just assigning the class to this point to keep that state
+                    $('#'+idOfSaved).attr('title', annotationCodeList.get(caab_code_id).get("code_name"));
+                    $('#'+idOfSaved).attr('caab_code', caab_object.get('caab_code'));
+                    $('#'+idOfSaved).tooltip("destroy");
+                    $('#'+idOfSaved).tooltip("show");
+
+                    GlobalEvent.trigger("image_points_updated", this);
+                    afterAllSavedCallback();
+                },
+                error: function (model, xhr, options) {
+                    if (theXHR.status == "201" || theXHR.status == "202") {
+                        alert("202");
+
+                        //show label on annotated point
+                        var idOfSaved = model.get("id");
+                        $('#'+idOfSaved).addClass('pointLabelledStillSelected'); //this means the point stays selected, we are just assigning the class to this point to keep that state
+                        $('#'+idOfSaved).attr('title', annotationCodeList.get(caab_code_id).get("code_name"));
+                        $('#'+idOfSaved).attr('caab_code', caab_object.get('caab_code'));
+                        $('#'+idOfSaved).tooltip("destroy");
+                        $('#'+idOfSaved).tooltip("show");
+
+                        GlobalEvent.trigger("image_points_updated", this);
+                        afterAllSavedCallback();
+
+                    } else if(theXHR.status == "401") {
+                        $.pnotify({
+                            title: 'You don\'t have permission to annotate this image.',
+                            text: theXHR.response,
+                            type: 'error', // success | info | error
+                            hide: true,
+                            icon: false,
+                            history: false,
+                            sticker: false
+                        });
+                    }
+                    else {
+                        $.pnotify({
+                            title: 'Failed to save your annotations to the server.',
+                            text: theXHR.response,
+                            type: 'error', // success | info | error
+                            hide: true,
+                            icon: false,
+                            history: false,
+                            sticker: false
+                        });
+                    }
+                }
+            });
+        });
+
+        GlobalEvent.trigger("annotation_triggered");
+    },
+    hidePoints: function () {
+        //loop through the points and hide them
+        points.each(function (point) {
+            var pointId = point.get('id');
+            var span = $('#'+pointId);
+
+            span.css('visibility', 'hidden');
+        });
+    },
+    showPoints: function () {
+         //loop through the points and show them
+        points.each(function (point) {
+            var pointId = point.get('id');
+            var span = $('#'+pointId);
+
+            span.css('visibility', 'visible');
+        });
+    },
+    zoomOn: function() {
+        $("#Image").elevateZoom({zoomWindowPosition: 1});
+    },
+    zoomOff: function() {
+        $.removeData($("#Image"), 'elevateZoom');
+        $('.zoomContainer').remove();
+    },
+    deselectPoints: function () {
+
+        //deselect any points that are labelled and still selected
+        $(".pointLabelledStillSelected").each(function (index, pointSpan) {
+            $(pointSpan).attr('class', 'pointAnnotated');
+        });
+
+        //deselect any points that are selected
+        $(".pointSelected").each(function(index, pointSpan) {
+            var theCaabCode = $(pointSpan).attr('caab_code');
+
+            if(theCaabCode === "") {
+                $(pointSpan).attr('class', 'pointNotAnnotated');
+            } else {
+                $(pointSpan).attr('class', 'pointAnnotated');
+            }
+        });
+
+        //refresh
+        this.refreshPointLabelsForImage();
+        this.disableAnnotationSelector();
+    },
+    enableAnnotationSelector: function(){
+        $('.AnnotationChooserBox').removeClass('disable');
+        $('a[href=#overall_root_node]').trigger('activate-node');
+    },
+    disableAnnotationSelector: function(){
+        $('.AnnotationChooserBox').addClass('disable');
+        this.closeAnnotationSelector();
+    },
+    closeAnnotationSelector: function() {
+        $('#annotation-chooser').find('ul').each(function(index,item){
+            if (index > 0){
+                $(item).hide();
+            }
+        });
+    }
+});
